939db60482338073b4a24ff1f988b3db8bd1af98
steve wates <swates@mobifed.com>
2021-08-21 15:03:10 +0100

    Added Exit Process

--- twostrings/main.ts
+++ twostrings/main.ts
@@ -1,6 +1,7 @@
 'use strict';
 import * as fs from 'fs';
 import { WriteStream, createWriteStream } from "fs";
+import { exit } from 'process';
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
 
@@ -20,7 +21,7 @@ function readLine(): string {
 
 main();
 
-async function twoStrings(s1: string, s2: string): Promise<string> {
+function twoStrings(s1: string, s2: string): string {
     // Write your code here
     s1 = s1.replace('\r','');;
     s2 = s2.replace('\r','');;
@@ -34,17 +35,21 @@ async function twoStrings(s1: string, s2: string): Promise<string> {
     return ret;
 }
 
-async function main() {
+function main() {
 
     const q: number = parseInt(readLine().trim(), 10);
 
     for (let qItr: number = 0; qItr < q; qItr++) {
         const s1: string = readLine();
+        if (!s1) break;
 
         const s2: string = readLine();
+        if (!s2) break;
 
-        const result: string = await twoStrings(s1, s2);
+        const result: string = twoStrings(s1, s2);
 
         console.log(result + '\n');
     }
+    console.log('Exit\n');
+    exit()
 }
